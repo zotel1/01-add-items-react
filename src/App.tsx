04b38ac40bf51d9e31c589e88ea28080e086646b
zotel1 <zotelsigel@gmail.com>
2024-07-18 23:52:19 -0300

    update, quedamos en mituto 33 midudev

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
-
+import { useState } from 'react'
 import './App.css'
 
+interface Item {
+    id: `${string}-${string}-${string}-${string}-${string}`
+    timestamp: number
+    text: string
+}
+
+const INITIAL_ITEMS: Item[] = [
+    {
+        id: crypto.randomUUID(),
+        timestamp: Date.now(),
+        text: 'Videojuegos',
+    },
+    {
+        id: crypto.randomUUID(),
+        timestamp: Date.now(),
+        text: 'Libros',
+    }
+]
+
 function App() {
+
+    const [items, setItems] = useState(INITIAL_ITEMS)
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const { elements } = event.currentTarget
+        const input = elements.namedItem('item')
+        const isInput = input instanceof HTMLInputElement
+        if (!isInput || input == null ) return
+
+    }
 
   return (
     <main>
         <aside>
       <h1>Prueba técnica de React</h1>
       <h2>Añadir y eliminar elementos de una lista</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
             Elemento a introducir:
             <input
